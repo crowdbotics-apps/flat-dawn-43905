709a08d192740344e8f4601bc1a002f18bf3052f
Crowdbotics <team@crowdbotics.com>
2023-10-12 19:59:08 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "flat-dawn-43905",
+  url: "https://flat-dawn-43905.botics.co",
+  api: "https://flat-dawn-43905.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
